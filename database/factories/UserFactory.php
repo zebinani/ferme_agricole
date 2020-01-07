1828e523794511e5fec8fdf06f512ed223e3faba
Diallo <masellouddiallo@mail.com>
2020-01-07 17:01:57 +0100

    Correct error on heroku

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,12 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name' => $faker->sellou,
+        'email' => "sellou@gmail.sn",
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'role'=>"Admin",
         'remember_token' => Str::random(10),
+        
     ];
 });

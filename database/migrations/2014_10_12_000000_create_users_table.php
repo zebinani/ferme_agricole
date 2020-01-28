c1a0e67d8c1c953946802e22e56bb1b172ea0e33
Diallo <masellouddiallo@mail.com>
2020-01-28 16:36:56 +0100

    correction5 route/web 28 janvier 2020

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->nullable();
+            $table->string('role')->default('UTILISATEUR');
             $table->rememberToken();
             $table->timestamps();
         });

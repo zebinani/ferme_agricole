8c7f6db84dd27a4efdfaf7d63299fff01689f12c
Diallo <masellouddiallo@mail.com>
2020-02-04 10:40:01 +0100

    correction layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,49 +1,41 @@
 <!DOCTYPE html>
 <html lang="fr">
+  <head>
+          <link href="{{asset('css/app.css')}}" rel="stylesheet" />
+          <link href="{{asset('css/all.css')}}" rel="stylesheet" />
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <meta name="description" content="">
+          <meta name="author" content="">
 
-      <head>
-        <link href="{{asset('css/app.css')}}" rel="stylesheet" />
-        <link href="{{asset('css/all.css')}}" rel="stylesheet" />
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
+          <title>FERME AGRICOLE</title>
 
-        <title>FERME AGRICOLE</title>
-
-        <!-- Bootstrap core CSS -->
-      </head>
+          <!-- Bootstrap core CSS -->
+  </head>     
 
   <body>
    
-    <header>
-        <!--
-          <div class="container justify-content-center">
-              <div class="row-12  fixed-top  heigth-45vw justify-content-center"> 
-                  <div class="col-12 "> PLATE-FORME DE GESTION AGRICOLE </div>
-              </div>
-          </div>
-        -->
-          <div class="container">
-              <div class="row  fixed-top text-align-center">
-                <div class="col-sm-12 bg-success">
-                  <div class="entete"><strong>  PLATE-FORME DE GESTION DE FERME AGRICOLE </strong>  </div>
+        <header>
             
-                </div>
+              <div class="container">
+                  <div class="row  fixed-top text-align-center">
+                    <div class="col-sm-12 bg-success">
+                      <div class="entete"><strong>  PLATE-FORME DE GESTION DE FERME AGRICOLE </strong>  </div>
+                
+                    </div>
+                  </div>
               </div>
-          </div>
-    </header>
+        </header>
  
   <main>
-  <!-- Navigation -->
-  <!-- <div id='titre'><h1>PLATE-FORME DE GESTION DE FERME</h1></div>-->
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
-  
-    <div class="container">
-    <a class="navbar-brand" href="/accueil">Accueil</a></button>
-    <a class="navbar-brand" href="/Command/create">Commander</a></button>
-    <a class="navbar-brand" href="/Contact/create">Contact</a></button>
-    <button type="button" class="btn btn-outline"><a class="navbar-brand" href="/accueil">Blog</a></button>
+ 
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+        
+          <div class="container">
+          <a class="navbar-brand" href="/accueil">Accueil</a></button>
+          <a class="navbar-brand" href="/Command/create">Commander</a></button>
+          <a class="navbar-brand" href="/Contact/create">Contact</a></button>
+          <a class="navbar-brand" href="/accueil">Blog</a>
       
       
       
@@ -52,72 +44,36 @@
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-       
-     <!--   <a class="navbar-brand" href="/Command/create"><button type="button" class="btn btn-outline-success"></button> </a> -->
-       
-        
-       
-        @can('Admin')
-              <!--
-                <a class="navbar-brand" href="/User"><button type="button" class="btn btn-outline-success">Utilisateurs</button> </a>
-                <a class="navbar-brand" href="/Product"> <button type="button" class="btn btn-outline-success">Produits</button> </a>
-                <a class="navbar-brand" href="/Employ"><button type="button" class="btn btn-outline-success">Employers</button> </a>
-                <a class="navbar-brand" href="/Client"><button type="button" class="btn btn-outline-success">Clients</button> </a>
-                <a class="navbar-brand" href="/Command"><button type="button" class="btn btn-outline-success">Commandes</button> </a>
-                -->
-       
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Menu
-            </a>
-           
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
-              <a class="dropdown-item" href="/Employ">Personnels</a>
-              <a class="dropdown-item" href="/Product">Produits</a>
-              <a class="dropdown-item" href="/Production">Production</a>
-              <a class="dropdown-item" href="/Materiel">Materiels</a>
-              <a class="dropdown-item" href="/Command">Commandes</a>
-              <a class="dropdown-item" href="/Ventes">Ventes</a>
-              <a class="dropdown-item" href="/Parcel">Parcelles</a>
-              <a class="dropdown-item" href="/Client">Clients</a>
-              <a class="dropdown-item" href="/Department">Departments</a>
-              <a class="dropdown-item" href="/User">Utilisateur</a>
-              <a class="dropdown-item" href="/Contact">contacts</a>
-              
-             
-            </div>
-      
-          </li>
 
-        @endcan
+      <div class="collapse navbar-collapse" id="navbarResponsive">
         
-       <!-- </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Blog
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-              <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
-              <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
-              <a class="dropdown-item" href="blog-post.html">Blog Post</a>
-            </div>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Other Pages
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-              <a class="dropdown-item" href="full-width.html">Full Width Page</a>
-              <a class="dropdown-item" href="sidebar.html">Sidebar Page</a>
-              <a class="dropdown-item" href="faq.html">FAQ</a>
-              <a class="dropdown-item" href="404.html">404</a>
-              <a class="dropdown-item" href="pricing.html">Pricing Table</a>
-            </div>
-          </li>
-          <li class="nav-item">
-          -->
+        <ul class="navbar-nav ml-auto">
+            @can('Admin')
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio"   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Menu
+                </a>
+              
+              <div class="dropdown-menu dropdown-menu-right"     aria-labelledby="navbarDropdownPortfolio">
+                  <a class="dropdown-item" href="/Employ">Personnels</a>
+                  <a class="dropdown-item" href="/Product">Produits</a>
+                  <a class="dropdown-item" href="/Production">Production</a>
+                  <a class="dropdown-item" href="/Materiel">Materiels</a>
+                  <a class="dropdown-item" href="/Command">Commandes</a>
+                  <a class="dropdown-item" href="/Ventes">Ventes</a>
+                  <a class="dropdown-item" href="/Parcel">Parcelles</a>
+                  <a class="dropdown-item" href="/Client">Clients</a>
+                  <a class="dropdown-item" href="/Department">Departments</a>
+                  <a class="dropdown-item" href="/User">Utilisateur</a>
+                  <a class="dropdown-item" href="/Contact">contacts</a>
+                  
+                </div>
+          
+              </li>
+
+            @endcan
+        
+          <!-- pour faire du backend -->
                @guest
                             
                             <li class="nav-item">
@@ -149,14 +105,14 @@
                                    
                                 </div>
                             </li>                            
-                        @endguest
-          </li>
-            </li>
-            </li>
-        </ul>
+                  @endguest
+              </li>
+              </li>
+              </li>
+          </ul>
       </div>
-    </div>
-  </nav>
+     </div>
+    </nav>
 
 
     <main>
